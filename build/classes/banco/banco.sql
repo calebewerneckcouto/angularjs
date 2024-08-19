563be5ae1c4790e92d4a922aec4c75a7209629b2
calebewerneckcouto <calebewerneck@hotmail.com>
2024-08-19 14:00:15 -0300

    angularjs

---
+++ build/classes/banco/banco.sql
@@ -0,0 +1,7 @@
+CREATE DATABASE cwcdcomb_angularjs_spring_restful
+  WITH OWNER = cwcdcomb
+       ENCODING = 'UTF8'
+       TABLESPACE = pg_default
+       LC_COLLATE = 'Portuguese_Brazil.1252'
+       LC_CTYPE = 'Portuguese_Brazil.1252'
+       CONNECTION LIMIT = -1;
